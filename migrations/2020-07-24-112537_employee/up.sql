bd6753f466f1753baee45fb1bb874d148945aca2
Kamran Amini <kam.cpp@gmail.com>
2020-07-24 14:52:01 +0000

    Change mobile field to contain 32 chars.

--- migrations/2020-07-24-112537_employee/up.sql
+++ migrations/2020-07-24-112537_employee/up.sql
@@ -5,5 +5,5 @@ CREATE TABLE employee (
     second_name VARCHAR(64) NOT NULL,
     username VARCHAR(16) NOT NULL UNIQUE,
     office_email VARCHAR(64) NOT NULL UNIQUE,
-    mobile VARCHAR(16) NOT NULL
+    mobile VARCHAR(32) NOT NULL
 )
