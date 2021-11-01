995a55e93b43e1299807e314e796ee81589e2121
Kamran Amini <kam.cpp@gmail.com>
2021-11-01 13:12:06 +0000

    Remove some code blocks in order to create the partial solution.

--- common/migrations/2021-06-08-214309_create_employee/up.sql
+++ common/migrations/2021-06-08-214309_create_employee/up.sql
@@ -1,6 +1,3 @@
 CREATE TABLE employee (
-    first_name VARCHAR(64) NOT NULL,
-    second_name VARCHAR(64) NOT NULL,
-    username VARCHAR(16) PRIMARY KEY,
-    totp_secret VARCHAR(64) NOT NULL
+    >> FILL HERE <<
 )
--- web-service/src/models.rs
+++ web-service/src/models.rs
@@ -2,16 +2,10 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct EmployeeModel {
-    #[serde(rename = "firstName")]
-    pub first_name: Option<String>,
-    #[serde(rename = "secondName")]
-    pub second_name: Option<String>,
-    pub username: Option<String>,
+    >> FILL HERE <<
 }
 
 #[derive(Debug, Deserialize)]
 pub struct IdentityVerificationRequestModel {
-    pub username: String,
-    #[serde(rename = "totpCode")] // Time-based One-Time Password
-    pub totp_code: String,
+    >> FILL HERE <<
 }
